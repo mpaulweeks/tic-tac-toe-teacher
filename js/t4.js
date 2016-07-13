b5c802a74a0ef090c78c4efd7d7b30dba73a5eab
mpaulweeks <mpaulweeks@gmail.com>
2016-07-12 22:40:06 -0400

    start api docs, copy dependencies to local

--- js/t4.js
+++ js/t4.js
@@ -272,7 +272,9 @@ function determinePincerMoves(freeSquares, mySquares){
     var pincerMoves = [];
     for (var i = 0; i < freeSquares.length; i++){
         var hypoMove = freeSquares[i];
-        var hypoFreeSquares = freeSquares.slice(0, i).concat(freeSquares.slice(i+1));
+        var hypoFreeSquares = (
+            freeSquares.slice(0, i).concat(freeSquares.slice(i+1))
+        );
         var hypoMySquares = mySquares.concat(hypoMove);
         if (determineWinningMoves(hypoFreeSquares, hypoMySquares).length >= 2){
             pincerMoves.push(hypoMove);
@@ -284,7 +286,9 @@ function determineSetupMoves(freeSquares, mySquares){
     var setupMoves = [];
     for (var i = 0; i < freeSquares.length; i++){
         var hypoMove = freeSquares[i];
-        var hypoFreeSquares = freeSquares.slice(0, i).concat(freeSquares.slice(i+1));
+        var hypoFreeSquares = (
+            freeSquares.slice(0, i).concat(freeSquares.slice(i+1))
+        );
         var hypoMySquares = mySquares.concat(hypoMove);
         if (determinePincerMoves(hypoFreeSquares, hypoMySquares).length >= 1){
             setupMoves.push(hypoMove);
@@ -307,15 +311,60 @@ return (
     api.getRandom(board.freeSquares)
 )`);
 
+var codeDocs = (
+`// board gives you access to the current game state
+board.freeSquares;
+board.mySquares;
+board.opponentSquares;
+
+// square gives you ids of all 9 squares
+square.TopLeft;
+square.TopCenter;
+square.TopRight;
+square.CenterLeft;
+square.Center;
+square.CenterRight;
+square.BottomLeft;
+square.BottomCenter;
+square.BottomRight;
+
+// example
+if (board.freeSquares.includes(square.Center)){
+    return square.Center;
+}
+
+// square also has lists of the corners and SIDES
+square.CORNERS;
+square.SIDES;
+
+// api has a number of helper functions
+
+api.checkForWin(squares);
+// Takes in a list of squares
+// Returns true/false is they contain a three-in-a-row
+
+api.getRandom(squares);
+// Takes in a list of squares
+// Returns one at random
+// Returns null if list is empty
+
+api.intersect(squares1, squares2);
+// Takes in two lists of squares
+// Returns a new list containing the squares present in both lists
+
+// example
+var freeCorners = api.intersect(board.freeSquares, square.CORNERS);
+`);
+
 function ticTacToe(){
     var editorCode = ace.edit("editor-code");
-    editorCode.setValue(simpleRobot);
+    editorCode.setValue(simpleRobot, -1);
     var editorStart = ace.edit("editor-start");
-    editorStart.setValue('function determineRobotMove(board, api, square) {');
+    editorStart.setValue('function determineRobotMove(board, api, square) {', -1);
     var editorEnd = ace.edit("editor-end");
-    editorEnd.setValue('}');
+    editorEnd.setValue('}', -1);
     var editorDocs = ace.edit("editor-docs");
-    editorDocs.setValue(expertRobot);
+    editorDocs.setValue(codeDocs, -1);
     [editorStart, editorEnd, editorDocs].forEach(function (editor){
         editor.setReadOnly(true);
     });
@@ -336,11 +385,11 @@ function ticTacToe(){
     }
     $('#run').click(loadRobot);
     $('#load-simple').click(function(){
-        editorCode.setValue(simpleRobot);
+        editorCode.setValue(simpleRobot, -1);
         loadRobot();
     });
     $('#load-expert').click(function(){
-        editorCode.setValue(expertRobot);
+        editorCode.setValue(expertRobot, -1);
         loadRobot();
     });
     loadRobot();
